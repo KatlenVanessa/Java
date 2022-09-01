db42b98c03cbeeef26b7156c607999e5b95df053
KatlenVanessa <katlenvanessa15@gmail.com>
2022-09-01 11:19:57 -0300

    up

--- src/matTest.java
+++ src/matTest.java
@@ -1,18 +1,18 @@
 public class matTest {
-    public static void main(String[] args){
-    System.out.println("O valor de pi é: " + Math.PI);
+    public static void main(String[] args) {
+        System.out.println("O valor de pi é: " + Math.PI);
 
-    System.out.println("O valor de E é: " + Math.E);
+        System.out.println("O valor de E é: " + Math.E);
 
-    System.out.println("'e' elevado ao quadrado = "+ (float)Math.exp(2));
-    
-    System.out.println("2 elevado ao cubo = " + (float)Math.pow(2, 3));
+        System.out.println("'e' elevado ao quadrado = " + (float) Math.exp(2));
 
-    System.out.println("A raiz quadrada de Pi é = "+ Math.sqrt( Math.PI ) );
+        System.out.println("2 elevado ao cubo = " + (float) Math.pow(2, 3));
 
-    System.out.println("O logaritmo natural de 10 é = "+ Math.log(10) );
-System.out.println("O logaritmo natural de 'e' é = "+ Math.log( Math.E )
-);
+        System.out.println("A raiz quadrada de Pi é = " + Math.sqrt(Math.PI));
+
+        System.out.println("O logaritmo natural de 10 é = " + Math.log(10));
+
+        System.out.println("O logaritmo natural de 'e' é = " + Math.log(Math.E));
 
     }
-    }
+}
