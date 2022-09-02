c7ac11c67e6ea88f447119cc8402749dee05e407
KatlenVanessa <katlenvanessa15@gmail.com>
2022-09-02 11:22:08 -0300

    up

---
+++ src/Herança.java
@@ -0,0 +1,8 @@
+public class Herança {
+    public static void main(String[] args) {
+        Filha filha = new Filha("Mariazinha");
+        /*a primeira coisa que um construtor de uma subclasse faz é
+invocar o construtor da superclasse. Só depois é que esse construtor da
+classe Filha faz suas ações. */
+    }
+}
---
+++ src/Pai.java
@@ -0,0 +1,13 @@
+public class Pai {
+
+    public String nomePai;
+
+    public Pai(){
+
+    this.nomePai = "Neil";
+    //System.out.println("Metodo construtor da classe Pai");
+
+    }
+    }
+
+ 
